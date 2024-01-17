fd0ce3e9417d59a54090e6660b0c1bf4c2eac2d8
Chrisglz14 <Christiang14@hotmail.com.ar>
2024-01-17 18:02:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MyPage from './components/MyPage'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Context API</h1>
+      <hr />
+      <MyPage />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyPage = () => {
+  return (
+    <>
+    <h2>Mi pagina</h2>
+    </>
+  )
+}
+
+export default MyPage
